93075d8eaa65daa8cc25a534e6003594f5b05dcc
Evin Aslan <h24evias@du.se>
2025-12-05 15:10:11 +0100

    upgift 4

---
+++ script.js
@@ -0,0 +1,7 @@
+const checkboxElement = document.getElementById("divStyle");
+
+const textFields = document.getElementsByClassName("textfield");
+
+const buttonElement = document.querySelector("#myButton");
+
+const divElement = document.getElementsByTagName("div")[1];

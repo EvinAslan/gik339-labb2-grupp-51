17010e58734db3a37628514cefb85ab1b4c4b019
Evin Aslan <h24evias@du.se>
2025-12-05 15:42:49 +0100

    uppgift 5 och 6

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Uppgift 4//
 const checkboxElement = document.getElementById("divStyle");
 
 const textFields = document.getElementsByClassName("textfield");
@@ -5,3 +6,38 @@ const textFields = document.getElementsByClassName("textfield");
 const buttonElement = document.querySelector("#myButton");
 
 const divElement = document.getElementsByTagName("div")[1];
+
+
+//Uppgift 5//
+
+function handleInput(e) {
+    console.log(e.traget);
+
+    const fieldName = e.target.name;
+
+    if (fieldName === "content") {
+        boxElement.innerHTML = e.target.value;
+    }
+
+}
+
+//Uppgift 6//
+checkboxElement.addEventListener("change", function(){
+    if (!colorField) return;
+    const chosenColor = colorField.value;
+    boxElement.style.backgroundColor = chosenColor;
+
+});
+
+
+
+for (let field of textFields){
+    field.addEventListener("input", handleInput);
+}
+
+
+buttonElement.addEventListener("click", function(){
+    boxElement.remove();
+});
+
+
